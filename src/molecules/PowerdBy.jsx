fcc6ea4c2222b7b8bb9433ac4ee5021e2d12dc1a
Rogier Mulders <rogier.mulders@xs4all.nl>
2024-05-23 10:06:31 +0200

    Test Hotel Allyourz

--- src/molecules/PowerdBy.jsx
+++ src/molecules/PowerdBy.jsx
@@ -5,7 +5,8 @@ export default function PowerdBy() {
 
   return <div className="grid">
     <div className="col-6 pb-8">
-      <span style={{cursor:'default', opacity:'0'}} onClick={()=> navigate('hotel-allyourz-middelburg')}>hay</span>
+      {/*Test Hotel Allyourz Middelburg*/}
+      <span style={{cursor:'default', opacity:'0'}} onClick={()=> navigate('/d7afc128-c19e-4dd5-8856-d4375baea240')}>ham</span>
     </div>
   <div className="col-6 pr-8 pb-8 text-right">
     <a href="https://allyourz.nl" target="_blank" rel="noreferrer">powered by allyourz</a>
