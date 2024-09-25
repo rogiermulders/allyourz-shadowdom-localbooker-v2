0f3f291d6a6db690cbb601a27495a9053831c6d1
Rogier Mulders <rogier.mulders@xs4all.nl>
2024-09-25 10:16:48 +0200

    WOW... Works!!!

--- src/themes/ThemeLoader.jsx
+++ src/themes/ThemeLoader.jsx
@@ -8,8 +8,8 @@ export default function ThemeLoader({ children,theme }) {
   const [unknownTheme, setUnknownTheme] = useState(false)
 
   /**
-   * !! this HAS to be done like this i think !!
-   * !! Vite needs to analyze the import statements !!
+   * !! this HAS to be done like this as far as I know !!
+   * !! Vite needs to analyze the import statements to create chunks !!
    * !! using a variable in the import statements will not work !!
    */
 
