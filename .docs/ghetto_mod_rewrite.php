f5c643c93d5e4f6e3412018bae88e2eafd758ea5
Rogier Mulders <rogier.mulders@xs4all.nl>
2024-09-26 13:08:21 +0200

    WIP

--- .docs/ghetto_mod_rewrite.php
+++ .docs/ghetto_mod_rewrite.php
@@ -7,8 +7,16 @@ $rules = [
     '/localbooker/localbooker-nb/',
     '/localbooker/lb-walcheren/',
 ];
+
+/** https://localbooker.nl */
+$rules = [
+    '/nl-nl/visit/zoek-en-boek/'
+];
+
 foreach ($rules as $rule) {
-    if (str_starts_with($_SERVER['REQUEST_URI'], $rule)) {
+    if (0 === strpos($_SERVER['REQUEST_URI'], $rule)) {
         $_SERVER['REDIRECT_URL'] = $_SERVER['REQUEST_URI'] = $rule;
     }
 }
+
+
