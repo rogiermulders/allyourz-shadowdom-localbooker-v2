2761a28320abd69e89151b707245669397fd13b1
Rogier Mulders <rogier.mulders@xs4all.nl>
2024-09-30 10:52:59 +0200

    zeeland.com hack

--- .docs/zeeland_com_hack.js
+++ .docs/zeeland_com_hack.js
@@ -1,4 +1,4 @@
-document.querySelector(`link[rel="alternate"][hreflang="${document.querySelector('html').lang}"]`).href.replace(',-w-,','').split('/').slice(-2).join('/')
+document.querySelector(`link[rel="alternate"][hreflang="${document.querySelector('html').lang}"]`).href.replace('/,-w-,','').split('/').slice(-2).join('/')
 
 
 
