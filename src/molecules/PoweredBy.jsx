907369471b31992cd4dc078aecb358cd338dd727
Rogier Mulders <rogier.mulders@xs4all.nl>
2024-10-28 16:47:15 +0100

    fixed en hostLocale

--- src/molecules/PoweredBy.jsx
+++ src/molecules/PoweredBy.jsx
@@ -1,16 +1,18 @@
 import { useNavigate } from 'react-router-dom'
 import { useEffect, useState } from 'react'
+import { useRecoilValue } from 'recoil'
+import recoilConfig from '../recoil/recoilConfig.js'
 
 export default function PoweredBy() {
   const [pv, setPv] = useState(false)
   const navigate = useNavigate()
-
+  const config = useRecoilValue(recoilConfig)
   useEffect(() => {
     const handle = setInterval(() => {
-      if(window.localbooker_preview !== pv) {
+      if (window.localbooker_preview !== pv) {
         setPv(window.localbooker_preview)
       }
-    },1000)
+    }, 1000)
     return () => {
       clearInterval(handle)
     }
@@ -20,12 +22,16 @@ export default function PoweredBy() {
     <div className="col-6 pb-8 pl-8">
       {/*Test Hotel Allyourz Middelburg*/}
       {pv &&
-        <span style={{cursor: 'pointer'}} onClick={() => navigate('/hotel-allyourz-middelburg')}>
+        <span style={{ cursor: 'pointer' }} onClick={() => navigate('/hotel-allyourz-middelburg')}>
           Hotel Allyourz Middelburg (Testing)
         </span>}
     </div>
     <div className="col-6 pr-8 pb-8 text-right">
-      <a href="https://allyourz.nl" target="_blank" rel="noreferrer">powered by allyourz</a>
+      {config.pid !== 'zeeland.com' ?
+        <a href="https://localbooker.nl" target="_blank" rel="noreferrer">powered by Localbooker</a>
+        :
+        <a href="https://allyourz.nl" target="_blank" rel="noreferrer">powered by allyourz</a>
+      }
     </div>
   </div>
 
