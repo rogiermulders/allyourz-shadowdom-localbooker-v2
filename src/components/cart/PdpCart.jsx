f609aef449b8dbf240be593c57e898347c52eee4
Rogier Mulders <rogier.mulders@xs4all.nl>
2025-01-18 14:55:50 +0700

    ZOEK & BOEK

--- src/components/cart/PdpCart.jsx
+++ src/components/cart/PdpCart.jsx
@@ -1,25 +1,27 @@
-import {useContext, useRef} from "react";
-import FakeDropdown from "../../molecules/FakeDropdown.jsx";
-import AdministrationCalendar from "../calendar/AdministrationCalendar.jsx";
-import {useRecoilValue} from "recoil";
-import selectorMainFilter from "../../recoil/selectors/selectorMainFilter";
-import {localeOption} from "primereact/api";
-import WhoContent from "../mainfilter/WhoContent.jsx";
-import {MainContext} from "../../contexts/MainContext";
-import {toEuro} from "../../services/money";
-import {Button} from "primereact/button";
-import CalendarDialog from "../calendar/CalendarDialog.jsx";
-import {gte} from "../../services/buttstrip";
+import { useContext, useRef } from 'react'
+import FakeDropdown from '../../molecules/FakeDropdown.jsx'
+import AdministrationCalendar from '../calendar/AdministrationCalendar.jsx'
+import { useRecoilValue } from 'recoil'
+import selectorMainFilter from '../../recoil/selectors/selectorMainFilter'
+import { localeOption } from 'primereact/api'
+import WhoContent from '../mainfilter/WhoContent.jsx'
+import { MainContext } from '../../contexts/MainContext'
+import { toEuro } from '../../services/money'
+import { Button } from 'primereact/button'
+import CalendarDialog from '../calendar/CalendarDialog.jsx'
+import { gte, lte } from '../../services/buttstrip'
+import { useNavigate } from 'react-router-dom'
 
 
-export default function PdpCart({administration, pdpScrollToFirstBookabe}) {
+export default function PdpCart({ administration, pdpScrollToFirstBookabe }) {
   const calendarDialogRef = useRef(null)
   const context = useContext(MainContext)
+  const navigate = useNavigate()
   const _t = context._t()
   const refWhen = useRef(null)
   const refWho = useRef(null)
 
-  const {checkIn, checkOut, adults, children, babies, pets} = useRecoilValue(selectorMainFilter)
+  const { checkIn, checkOut, adults, children, babies, pets } = useRecoilValue(selectorMainFilter)
 
   const monthNames = localeOption('monthNames')
 
@@ -32,48 +34,74 @@ export default function PdpCart({administration, pdpScrollToFirstBookabe}) {
   const whoTitle = tot + ' ' + _t.labels[tot > 1 ? 'guests' : 'guest'] +
     (pets ? `, ${pets} ${_t.labels[pets > 1 ? 'pets' : 'pet']}` : '')
 
+  const butt= () => <Button
+    icon="pi pi-angle-double-left"
+    style={{ backgroundColor: 'var(--surface-a)' }}
+    iconPos="lef"
+    className="w100"
+    outlined
+    label={
+      _t.labels.search_and_book || 'Terug naar ZOEK & BOEK'
+    }
+    onClick={() => navigate('/')}
+  />
   /**
    * Return
    */
-  return <div className="text-color p-8">
-    <CalendarDialog ref={calendarDialogRef}/>
-    {/*Selecteer Datums*/}
-    {gte('md') && <div className="h3">{_t.page_pdp.select_dates}</div>}
+  return <>
 
-    {/*Aankomst & vertrek*/}
-    <div className="h5 mt-4 mb-2">{_t.page_pdp.checkin_checkout}</div>
-    <FakeDropdown ref={refWhen} placeholder={whenTitle} onOpen={() => refWho.current.close()}>
-      <div style={{margin: 'auto', width: 'max-content', marginTop:'0.2em'}}>
-        <AdministrationCalendar
-          administration_id={administration.id}
-          onClose={() => refWhen.current.close()}
-        />
+    <CalendarDialog ref={calendarDialogRef} />
+
+    <div className="text-color p-8" style={{position:'relative'}}>
+
+      {gte('md') && <div style={{position:'absolute', top:'-1.6em', right:'1em', width:'18em'}}>
+        {butt()}
+      </div>}
+
+      {/*Back to home*/}
+      {lte('sm') && <div className="mt-3 mb-8">
+        {butt()}
+      </div>}
+
+
+      {/*Selecteer Datums*/}
+      {gte('md') && <div className="h3">{_t.page_pdp.select_dates}</div>}
+
+      {/*Aankomst & vertrek*/}
+      <div className="h5 mt-4 mb-2">{_t.page_pdp.checkin_checkout}</div>
+      <FakeDropdown ref={refWhen} placeholder={whenTitle} onOpen={() => refWho.current.close()}>
+        <div style={{ margin: 'auto', width: 'max-content', marginTop: '0.2em' }}>
+          <AdministrationCalendar
+            administration_id={administration.id}
+            onClose={() => refWhen.current.close()}
+          />
+        </div>
+      </FakeDropdown>
+
+      {/*Hoeveel personen*/}
+      <div className="h5 mt-4 mb-2">{_t.page_pdp.how_many}</div>
+      <FakeDropdown ref={refWho} placeholder={whoTitle} onOpen={() => refWhen.current.close()}>
+        <WhoContent administration={administration} usedIn={'PdpCart'} />
+      </FakeDropdown>
+
+      {/*Vanaf /nacht*/}
+      <div className="flex-center mt-4">
+        <div className="text-sm-4 mt-2">{_t.labels.from_night}</div>
+        <div className="text-sm-8 ml-2">{toEuro(context.cheapest, true)}</div>
       </div>
-    </FakeDropdown>
 
-    {/*Hoeveel personen*/}
-    <div className="h5 mt-4 mb-2">{_t.page_pdp.how_many}</div>
-    <FakeDropdown ref={refWho} placeholder={whoTitle} onOpen={() => refWhen.current.close()}>
-      <WhoContent administration={administration} usedIn={'PdpCart'}/>
-    </FakeDropdown>
-
-    {/*Vanaf /nacht*/}
-    <div className="flex-center mt-4">
-      <div className="text-sm-4 mt-2">{_t.labels.from_night}</div>
-      <div className="text-sm-8 ml-2">{toEuro(context.cheapest, true)}</div>
+      {/*Button*/}
+      <div className="text-center mt-4">
+        <Button label={checkIn && checkOut ? _t.labels.choose_your_room : _t.labels.select_dates}
+                onClick={() => {
+                  if (checkIn && checkOut) {
+                    pdpScrollToFirstBookabe()
+                  } else {
+                    calendarDialogRef.current.open(administration.id)
+                  }
+                }}
+                style={{ width: '60%' }} />
+      </div>
     </div>
-
-    {/*Button*/}
-    <div className="text-center mt-4">
-      <Button label={checkIn && checkOut ? _t.labels.choose_your_room : _t.labels.select_dates}
-              onClick={() => {
-                if (checkIn && checkOut) {
-                  pdpScrollToFirstBookabe()
-                } else {
-                  calendarDialogRef.current.open(administration.id)
-                }
-              }}
-              style={{width: '60%'}}/>
-    </div>
-  </div>
+  </>
 }
--- src/services/scrollIntoViewWithOffset.js
+++ src/services/scrollIntoViewWithOffset.js
@@ -6,13 +6,15 @@ const scrollIntoViewWithOffset =  (nodeOrRef, offset, scroll) => {
 
   if(!(nodeOrRef instanceof HTMLElement)) nodeOrRef = nodeOrRef.current
 
+  const fontSize = parseFloat(getComputedStyle(document.getElementById(import.meta.env.VITE_APP_ROOT)).fontSize);
+
   setTimeout(() => {
     window.scrollTo({
       behavior: 'smooth',
       top:
         nodeOrRef.getBoundingClientRect().top -
         document.body.getBoundingClientRect().top -
-        offset,
+        (offset + (1.5 * fontSize)),
     })
   }, 100) // Dunno.. without this one things are *not* working
 
