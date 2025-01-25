75a693ac51a513fe625c863241e3457e50a0dc53
Rogier Mulders <rogier.mulders@xs4all.nl>
2025-01-25 12:10:16 +0700

    WIP

--- src/components/cart/CartTermsAndConditions.jsx
+++ src/components/cart/CartTermsAndConditions.jsx
@@ -9,61 +9,55 @@ export default function CartTermsAndConditions({ admin }) {
   const context = useContext(MainContext)
   const _t = context._t().page_pdp
 
-  const cancelHead = admin.cancellationThreshold === '0_document' ? _t.cancelation_rules : _t.free_cancelation
+  const items = []
 
-  console.log(admin.type)
+  /**
+   * Cancellation policies
+   */
+  if (admin.cancellation && !!admin.cancellationThreshold) {
+    const cancelHead = admin.cancellationThreshold === '0_document' ? _t.cancelation_rules : _t.free_cancelation
 
-  const cancelText = () => {
-    if (admin.cancellationThreshold.includes('hours')) {
-      return _t.cancellation_threshold_hours?.replace('{{hours}}', parseInt(admin.cancellationThreshold).toString())
+    const cancelText = () => {
+      if (admin.cancellationThreshold.includes('hours')) {
+        return _t.cancellation_threshold_hours?.replace('{{hours}}', parseInt(admin.cancellationThreshold).toString())
+      }
+      if (admin.cancellationThreshold.includes('days')) {
+        return _t.cancellation_threshold_days?.replace('{{days}}', parseInt(admin.cancellationThreshold).toString())
+      }
+      return _t[`cancellation_default_text_STAYS`]
     }
-    if (admin.cancellationThreshold.includes('days')) {
-      return _t.cancellation_threshold_days?.replace('{{days}}', parseInt(admin.cancellationThreshold).toString())
-    }
-    return _t[`cancellation_default_text_STAYS`]
+    // Add to items
+    items.push({ head: cancelHead, name: cancelText(), content: admin.cancellation })
   }
 
-  const payInAdvance = () => {
-    if (admin.depositPercentage) {
-      return _t.pay_in_advance_situation_percentage?.replace('{{percentage}}', admin.depositPercentage.toString())
+  /**
+   * Pay in advance
+   */
+  if (admin.depositPercentage < 100 && admin.payInAdvance) {
+    const payInAdvance = () => {
+      if (admin.depositPercentage) {
+        return _t.pay_in_advance_situation_percentage?.replace('{{percentage}}', admin.depositPercentage.toString())
+      }
+      if (admin.payInAdvanceSituation) {
+        return _t.pay_in_advance_situation_email
+      }
+      if (admin.payInAdvanceSituation === 'arrival') {
+        return _t.pay_in_advance_situation_arrival
+      }
+      return _t.pay_in_advance_situation_default_text
     }
-    if (admin.payInAdvanceSituation) {
-      return _t.pay_in_advance_situation_email
-    }
-    if (admin.payInAdvanceSituation === 'arrival') {
-      return _t.pay_in_advance_situation_arrival
-    }
-    return _t.pay_in_advance_situation_default_text
+
+    // Add to items
+    items.push({ head: _t.pdp_no_deposit, name: payInAdvance(), content: admin.payInAdvance })
   }
 
 
-  const tc = [
-    {
-      head: cancelHead,
-      name: cancelText(),
-      content: admin.cancellation
-    },
-    {
-      head: _t.pdp_no_deposit,
-      name: payInAdvance(),
-      content: admin.payInAdvance
-    }
-  ]
-
-
-  const content = (text) => <div className="p-8" style={{
-    whiteSpace: 'pre-line',
-    lineHeight: '1.2em',
-    fontSize: '1.1em'
-  }}>
-    {text}
-  </div>
-
+  /**
+   * HTML
+   */
   return <>
     <ForwardDialog ref={ref} />
-    {tc.map((item, i) => {
-
-
+    {items.map((item, i) => {
       return <div key={i} className="flex mb-4">
         <div>
           <Icon name="check-circle" size="1.25em" color="#8ccbc8" />
@@ -72,9 +66,16 @@ export default function CartTermsAndConditions({ admin }) {
           <div className="font-bold">{item.head}</div>
           <button
             onClick={() => {
+              // Open the popup
               ref.current.open({
                 header: item.head,
-                content: content(item.content)
+                content: <div className="p-8" style={{
+                  whiteSpace: 'pre-line',
+                  lineHeight: '1.2em',
+                  fontSize: '1.1em'
+                }}>
+                  {item.content}
+                </div>
               })
             }}
             className="no-style mt-1 flex">
