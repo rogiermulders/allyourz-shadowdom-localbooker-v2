7bd17700b142bacb3e7b59b1181033c32c677d8e
Rogier Mulders <rogier.mulders@xs4all.nl>
2025-01-27 18:05:43 +0700

    WIP

--- src/components/administration/Administration.jsx
+++ src/components/administration/Administration.jsx
@@ -141,12 +141,12 @@ export default function Administration({ administration }) {
       </div>
 
       {/*Speciale kenmerken*/}
-      {!lte('xs') && <div className="mt-8 ml-4 mr-4">
+      {false && !lte('xs') && <div className="mt-8 ml-4 mr-4">
         <hr />
         <h4 className="mb-4 mt-8">{_t.page_pdp.characteristics}</h4>
         <CharacteristicItems facilityGroups={administration.facilityGroups} />
       </div>}
-      <div className="mt-8 m-4">
+      {false && <div className="mt-8 m-4">
         <Button outlined label={_t.page_pdp.show_all_facilities} onClick={() => {
           dialogRef.current.open(
             {
@@ -157,7 +157,7 @@ export default function Administration({ administration }) {
               </div>
             })
         }} />
-      </div>
+      </div>}
     </div>
   </>
 
